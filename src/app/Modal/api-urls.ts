b82c2e22dbbaae4ebacc9c8a7d74cb1b1780174f
Nawaz Khan <nawazkhan4032@gmail.com>
2025-11-24 23:51:41 +0530

    Dashboard Data Added

---
+++ src/app/Modal/api-urls.ts
@@ -0,0 +1,11 @@
+export const API_URLS = {
+  baseURL: 'https://api.freeprojectapi.com/api/FeesTracking/',
+  All_API_URLS: {
+    GET_BATCHES: 'batches',
+    ADD_NEW_ENROLLMENT: 'addNewEnrollment',
+    GET_ALL_ENROLLMENTS: 'getAllEnrollments',
+    UPDATE_ENROLLMENT: 'updateEnrollment',
+    DELETE_ENROLLMENT: 'SoftDeleteById',
+    GET_DASHBOARD_STATS: 'GetDashboardStats',
+  },
+};
--- src/app/service/myservice.ts
+++ src/app/service/myservice.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { Batch } from '../enrollment-dialog/enrollment-dialog';
 import { Observable } from 'rxjs';
 import { Idashboard } from '../Modal/modal';
+import { API_URLS } from '../Modal/api-urls';
 
 @Injectable({
   providedIn: 'root',
@@ -11,37 +12,38 @@ export class Myservice {
   private http: HttpClient = inject(HttpClient);
 
   getbatches(): Observable<Batch[]> {
-    return this.http.get<Batch[]>('https://api.freeprojectapi.com/api/FeesTracking/batches');
+    return this.http.get<Batch[]>(API_URLS.baseURL + API_URLS.All_API_URLS.GET_BATCHES);
   }
 
   addNewEnrollment(enrollmentData: any) {
     return this.http.post(
-      'https://api.freeprojectapi.com/api/FeesTracking/addNewEnrollment',
+      API_URLS.baseURL + API_URLS.All_API_URLS.ADD_NEW_ENROLLMENT,
       enrollmentData
     );
   }
 
   getAllEnrollments(): Observable<any> {
-    return this.http.get('https://api.freeprojectapi.com/api/FeesTracking/getAllEnrollments');
+    return this.http.get(API_URLS.baseURL + API_URLS.All_API_URLS.GET_ALL_ENROLLMENTS);
   }
 
   updateEnrollment(enrollmentData: any): Observable<any> {
     return this.http.put(
-      'https://api.freeprojectapi.com/api/FeesTracking/updateEnrollment?id=' +
+      API_URLS.baseURL +
+        API_URLS.All_API_URLS.UPDATE_ENROLLMENT +
+        '?id=' +
         enrollmentData.enrollmentId,
+
       enrollmentData
     );
   }
 
   deleteEnrollment(enrollmentId: number): Observable<any> {
     return this.http.delete(
-      'https://api.freeprojectapi.com/api/FeesTracking/SoftDeleteById?id=' + enrollmentId
+      API_URLS.baseURL + API_URLS.All_API_URLS.DELETE_ENROLLMENT + '?id=' + enrollmentId
     );
   }
 
   getDashboard(): Observable<Idashboard> {
-    return this.http.get<Idashboard>(
-      'https://api.freeprojectapi.com/api/FeesTracking/GetDashboardStats'
-    );
+    return this.http.get<Idashboard>(API_URLS.baseURL + API_URLS.All_API_URLS.GET_DASHBOARD_STATS);
   }
 }
